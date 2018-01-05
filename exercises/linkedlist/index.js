e04648cd4e0fec486eb1aab2322d49e5284f93c4
Jason Rosso <jasonrosso@gmail.com>
2018-01-05 01:03:58 -0500

    remove node at

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -38,8 +38,9 @@ class LinkedList {
         return this.head
     }
     getLast(){
+        if(!this.head) return;
         let currentNode=this.head
-    
+        
     while(currentNode.next){
       currentNode=currentNode.next
     }
@@ -71,6 +72,86 @@ class LinkedList {
         }
         previousNode.next=null
       }
+    insertLast(data){
+        let newNode = new Node(data)
+        if(!this.head) {
+            this.head= newNode
+            return;
+          }
+        let last = this.getLast()
+        last.next=newNode 
+    }
+    getAt(targetIndex){ //remember linked lists are 0 indexed!
+        
+        let currentNode=this.head
+        let currentIndex=0
+        
+          while(currentNode){
+            if(currentIndex===targetIndex){
+              return currentNode
+            }
+            currentNode=currentNode.next
+            currentIndex++
+          }
+          return null
+        }
+    removeAt(targetIndex){
+        if(!this.head) return;
+        if(targetIndex===0){
+          this.head=this.head.next
+          return;
+        }
+        let currentNode=this.head
+        let currentIndex=0
+        let previousNode;
+        while(currentNode){
+          if(targetIndex===currentIndex){
+            let deletedNex= currentNode.next
+            previousNode.next=currentNode.next
+            return;
+          }
+          previousNode=currentNode
+          currentNode=currentNode.next
+          currentIndex++
+        }
+        previousNode.next=null
+    }
+    insertAt(data,targetIndex){
+        const newNode= new Node(data)
+        if(!this.head) {
+            this.head=newNode
+            return;
+            }
+        
+        if(targetIndex===0){
+            let currentTemp=this.head
+            this.head=newNode
+            this.head.next=currentTemp
+            return;
+            }
+            let currentNode=this.head
+            let currentIndex=0
+            let previousNode;
+            
+        while(currentNode){
+            if(currentIndex===targetIndex){
+                let currentTemp = currentNode
+                previousNode.next=newNode
+                currentNode=newNode
+                currentNode.next=currentTemp
+                return;
+              }
+            previousNode=currentNode
+            currentNode=currentNode.next
+            //   if(!currentNode){
+            //     previousNode.next=newNode
+            //     return;
+            //   }
+             currentIndex++
+            }
+            previousNode.next=newNode //<<<<--alt to the above if statement
+          }
+      
 }
 
 module.exports = { Node, LinkedList };
